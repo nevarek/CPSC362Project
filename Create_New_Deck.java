6d9d9734f022d8a7436eb16eba224ec58024f220
daverave13 <dslusser13@gmail.com>
2016-09-24 11:45:44 -0700

    Add files via upload

---
+++ Create_New_Deck.java
@@ -0,0 +1,13 @@
+package com.niagarawater.dslusser.cpsc362project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Create_New_Deck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create__new__deck);
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.niagarawater.dslusser.cpsc362project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public Button buttonNew;
+
+    public void newButtonPressed(View view ) {
+        buttonNew = (Button)findViewById(R.id.buttonNew);
+        buttonNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeView = new Intent(MainActivity.this,Create_New_Deck.class);
+                startActivity(changeView);
+            }
+        });
+    }
+}
